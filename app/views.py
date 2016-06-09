c46ab8945cbb3e93428a80011ec4277aba604d8f
Nikolz <nikolzp@gmail.com>
2016-06-09 18:59:37 +0300

    1

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
  # -*- coding: utf-8 -*-
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
 from forms import LoginForm
+from models import User, ROLE_USER, ROLE_ADMIN
 
 @app.route('/')
 @app.route('/index')
@@ -15,9 +17,12 @@ def index():
 
 
 @app.route('/login', methods = ['GET', 'POST'])
+@oid.loginhandler
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title = 'Sign In', form = form)
+  if g.user is not None and g.user.is_authenticated():
+      return redirect(url_for('index'))
+  form = LoginForm()
+  if form.validate_on_submit():
+      session['remember_me'] = form.remember_me.data
+      return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])
+  return render_template('login.html', title = 'Sign In', form = form, providers = app.config['OPENID_PROVIDERS'])
